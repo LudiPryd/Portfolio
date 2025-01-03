abe28a2d308b96921b2c9461061bcfdc5e436752
ludi <ludipriyadi@gmail.com>
2025-01-03 11:53:22 +0700

    [add] padding bottom

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="bg-black ">
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="relative isolate px-6 pt-14 lg:px-8 pb-40">
         <div
           aria-hidden="true"
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
